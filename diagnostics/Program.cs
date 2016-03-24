fbb49eb4e57ff953cf40cb7f8279ac13cf711911
lemoce <cerencio@yahoo.com.br>
2016-03-24 18:10:36 -0300

    diagnostics project

---
+++ diagnostics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Threading;
+
+namespace diag
+{
+	class MainClass
+	{
+		private static TraceSource mySource = 
+			new TraceSource ("diag");
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine (ConfigurationManager.AppSettings ["myVar"]);
+			Activity1 ();
+			mySource.Flush ();
+			mySource.Close ();
+			return;
+		}
+
+		static void Activity1 ()
+		{
+			mySource.TraceEvent (TraceEventType.Error, 1, "Error message");
+			mySource.TraceInformation ("teste");
+			mySource.TraceEvent (TraceEventType.Warning, 2, "Warning message");
+		}
+	}
+}
